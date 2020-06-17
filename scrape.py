02239e32629692a29afc697f199b19730b4243ea
AbdulZor <57222786+AbdulZor@users.noreply.github.com>
2020-06-17 15:55:50 +0200

    initialization webscraper project

--- scrape.py
+++ scrape.py
@@ -1,3 +1,7 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
-def main():
-    print("Start scraping hckrnews.com")
+driver = webdriver.Chrome("C:/Users/Eigenaar/AppData/Local/Google/Chrome/Application/chromedriver.exe")
+driver.get("https://hckrnews.com/")
+
+print("Start scraping hckrnews.com")
