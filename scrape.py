3d2d4c1c8f5bf94df3536be73a4f11cbca3d4181
AbdulZor <57222786+AbdulZor@users.noreply.github.com>
2020-06-17 17:05:44 +0200

    initialization with selenium and bs4 dependencies

--- scrape.py
+++ scrape.py
@@ -4,4 +4,19 @@ from bs4 import BeautifulSoup
 driver = webdriver.Chrome("C:/Users/Eigenaar/AppData/Local/Google/Chrome/Application/chromedriver.exe")
 driver.get("https://hckrnews.com/")
 
+content = driver.page_source
+soup = BeautifulSoup(content, features="html.parser")
+
+for row in soup.find_all("li", {"class": "entry row"}):
+
+    commentElement = row.find("span", {"class": "comments"})
+    pointsElement = row.find("span", {"class": "points"})
+    titleElement = row.find("a", {"class": "story", "data-date": None})
+
+    if commentElement is not None and commentElement.text != "":
+        comments = commentElement.text
+        points = pointsElement.text
+        title = titleElement.text
+        print(comments + ", " + points + ", TITLE: " + title)
+
 print("Start scraping hckrnews.com")
