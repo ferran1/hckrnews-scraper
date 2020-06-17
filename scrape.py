f0baf6ad2a8a0e71fd64475348dca1b6d772db38
AbdulZor <57222786+AbdulZor@users.noreply.github.com>
2020-06-17 17:08:36 +0200

    deleted get-pip and added it in .gitignore

--- scrape.py
+++ scrape.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome("C:/Users/Eigenaar/AppData/Local/Google/Chrome/Application/chromedriver.exe")
-driver.get("https://hckrnews.com/")
-
 print("Start scraping hckrnews.com")
