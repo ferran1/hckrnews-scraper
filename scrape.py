bced2f7b6a8403eafad073cc44a1ebf0224531ac
AbdulZor <57222786+AbdulZor@users.noreply.github.com>
2020-06-18 17:31:51 +0200

    finished finding comments,points,title,link values from webpage - DEAD declared articles are neglected

--- scrape.py
+++ scrape.py
@@ -3,12 +3,26 @@ from bs4 import BeautifulSoup
 
 result = requests.get("https://hckrnews.com")
 
-print(result.status_code)
+result_status = result.status_code
 
 html = result.content
 
 soup = BeautifulSoup(html, 'lxml')
 
-links = soup.find_all("a")
 
-print(links)
+def is_not_dead(element):
+    return 'dead' not in element['class']
+
+
+for row in soup.find_all("li", class_="entry row"):
+
+    comment_element = row.find("span", class_="comments")
+    points_element = row.find("span", class_="points")
+    title_element = row.find("a", {"class": "story", "data-date": None})
+
+    if comment_element and points_element and title_element is not None \
+            and is_not_dead(title_element):
+        comments = comment_element.text
+        points = points_element.text
+        title = title_element.text
+        link = title_element["href"]
