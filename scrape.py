767f82d48b2e2fd532cd04e4dd67fe3145a73d39
ferran1 <ferrantl@live.nl>
2020-06-18 19:24:47 +0200

    scrape hckrnews finished

--- scrape.py
+++ scrape.py
@@ -14,12 +14,21 @@ articles = soup.find_all("a", class_="link span15 story")
 # Keywords to look for in the titles
 keywords = ["Ubuntu", "Microservices", "Encryption", "Academia"]
 
-articles_found = []
+# articles which are declared DEAD have a 'dead' value in the class attribute
+# return False if article is declared DEAD else True
+def is_not_dead(element):
+    return 'dead' not in element
 
 for article in articles:
     for keyword in keywords:
         if keyword in article.text:
-            articles_found.append([article.text, article.attrs['href']])
+            title = article.text
+            url = article.attrs['href']
+            if title and url is not None \
+                    and is_not_dead(title):
+                        print(title)
+                        print(url)
+                        # Send mail
 
-# print(articles)
-print(articles_found)
+
+
