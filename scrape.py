14efa8a41da3d49da8f55dd53bf8d77512181ffb
AbdulZor <57222786+AbdulZor@users.noreply.github.com>
2020-06-23 13:02:52 +0200

    added mailing functionality

--- scrape.py
+++ scrape.py
@@ -1,6 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+# import for Mail purposes
+import os
+import smtplib
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ['GMAIL_EMAIL']
+EMAIL_PASSWORD = os.environ.get('GMAIL_PASSWORD_PYTHON_TEST')
+print(EMAIL_ADDRESS)
+print(EMAIL_PASSWORD)
+
+contacts = ['zorabdul@gmail.com', 'ferran1004@gmail.com ']
+
 result = requests.get("https://hckrnews.com")
 
 result_status = result.status_code
@@ -8,6 +20,21 @@ result_status = result.status_code
 html = result.content
 
 soup = BeautifulSoup(html, 'lxml')
+
+
+# Sends e-mail with array of html string
+def send_email(articles_html: []):
+    msg = EmailMessage()
+    msg['Subject'] = 'News from hckernews! '
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = EMAIL_ADDRESS
+    # msg['To'] = ", ".join(contacts)
+
+    msg.add_alternative("<br>".join(articles_html), subtype="html")
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
 
 
 # artciles which are declared DEAD have a 'dead' value in the class attribute
@@ -19,15 +46,20 @@ def is_not_dead(element):
 articles = soup.find_all("a", class_="link span15 story")
 
 # Keywords to look for in the titles
-keywords = ["Ubuntu", "Microservices", "Encryption", "Academia"]
+keywords = ["Ubuntu", "Microservices", "Encryption", "Academia", "Jira", "RSS"]
+
+found_articles = []
 
 for article in articles:
     for keyword in keywords:
         if keyword in article.text:
             title = article.text
             url = article.attrs['href']
+
             if title and url is not None \
-                    and is_not_dead(title):
-                print(title)
-                print(url)
-                # Send mail
+                    and is_not_dead(article):
+                found_articles.append(str(article))
+                print(found_articles)
+
+# Send mail
+send_email(found_articles)
